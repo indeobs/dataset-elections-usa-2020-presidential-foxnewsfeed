95675446772342ca8f27e6dcf8ba5038b2102893
IndeObs <github@indeobs.org>
2020-12-13 19:48:48 +0100

    Create dir in rsync.

--- rsync.sh
+++ rsync.sh
@@ -2,4 +2,5 @@
 
 SERVER=$1
 
+mkdir -p feeds-elections.foxnews.com/archive/politics/elections/
 rsync -av $SERVER:feeds-elections.foxnews.com/archive/politics/elections/2020 feeds-elections.foxnews.com/archive/politics/elections/
